10b06e2fafa4db15f3b7aa132b2e8271628c75b8
nnadeysha <nadeysha@mail.ru>
2021-08-08 00:03:47 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+ if(!array || !array.length) return 0;
+ let min = Math.min.apply(null, array);
+ return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || !array.length) return 0;
+    let max = array.reduce(function(a, b) {
+        return Math.max(a, b);
+    });
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || !array.length) return 0;
+    return array.reduce((a, b) => a + b)/array.length;
 }
